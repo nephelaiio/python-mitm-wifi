102e82cda7e8800fd1ac6f0a4c474ad0d6c66dfe
Ted Cook <teodoro.cook@gmail.com>
2023-05-24 21:26:42 -0600

    Update hostapd and iptables configuration

--- mitm_wifi/packages.py
+++ mitm_wifi/packages.py
@@ -6,6 +6,7 @@ from .logger import logger
 
 from netaddr import IPNetwork
 from textwrap import dedent
+from pyroute2 import IPDB
 
 services: list[str] = ["hostapd", "dnsmasq"]
 packages: list[str] = []
@@ -42,15 +43,17 @@ def start_hostapd(iface: str, ssid: str = "mitm", password: str = "12345678"):
         f"""
         interface={iface}
         driver=nl80211
-        country_code=CR
         ssid={ssid}
         hw_mode=g
-        wpa=2
-        wpa_passphrase={password}
-        wpa_key_mgmt=WPA-PSK
         channel=6
         macaddr_acl=0
         auth_algs=1
+        ignore_broadcast_ssid=0
+        wpa=2
+        wpa_passphrase={password}
+        wpa_key_mgmt=WPA-PSK
+        wpa_pairwise=TKIP
+        rsn_pairwise=CCMP
         """,
     )
     run(f"systemctl start {service_name}@{iface}")
@@ -58,6 +61,10 @@ def start_hostapd(iface: str, ssid: str = "mitm", password: str = "12345678"):
 
 
 def start_dnsmasq(iface: str, ipaddr: str):
+    ip = IPDB()
+    iface_in = iface
+    iface_default = ip.routes["default"]["oif"]
+    iface_out = ip.interfaces[iface_default]["ifname"]
     service_name = "dnsmasq"
     service_default = f"/etc/default/dnsmasq.{iface}"
     service_config = f"/etc/dnsmasq.{iface}.conf"
@@ -65,17 +72,17 @@ def start_dnsmasq(iface: str, ipaddr: str):
         network = IPNetwork(ipaddr)
         gateway = network[1]
         iprange = f"{network[2]},{network[-2]}"
-        logger.info(f"starting {service_name} for interface {iface}")
+        logger.info(f"starting {service_name} for interface {iface_in}")
         write_config(
             service_default,
             f"""
-            DNSMASQ_OPTS="--conf-file=/etc/dnsmasq.{iface}.conf --bind-interfaces"
+            DNSMASQ_OPTS="--conf-file=/etc/dnsmasq.{iface_in}.conf --bind-interfaces"
             """,
         )
         write_config(
             service_config,
             f"""
-            interface={iface}
+            interface={iface_in}
             dhcp-range={iprange},12h
             dhcp-option=3,{gateway}
             dhcp-option=6,{gateway}
@@ -85,16 +92,23 @@ def start_dnsmasq(iface: str, ipaddr: str):
             log-dhcp
             """,
         )
-        run(f"ip address add {gateway}/24 dev {iface}")
+        run(f"ip address add {gateway}/24 dev {iface_in}")
     except:
         logger.error(f"invalid ip address: {ipaddr}")
         exit(1)
-    run(f"systemctl start {service_name}@{iface}")
-    run(f"iptables -t nat -A POSTROUTING -o {iface} -j MASQUERADE")
+    run(f"systemctl start {service_name}@{iface_in}")
+    iptables_rule_nat = f"iptables -t nat -A POSTROUTING -o {iface_out} -j MASQUERADE"
+    iptables_rule_out = f"iptables -A FORWARD -i {iface_in} -o {iface_out} -j ACCEPT"
+    iptables_rule_in = f"iptables -A FORWARD -i {iface_out} -o {iface_in} -j ACCEPT"
+    run(iptables_rule_nat)
+    run(iptables_rule_out)
+    run(iptables_rule_in)
     add_handler(
         lambda: stop_service(service_name, iface, [service_default, service_config])
     )
-    add_handler(lambda: run(f"iptables -t nat -D POSTROUTING -o {iface} -j MASQUERADE"))
+    add_handler(lambda: run(iptables_rule_nat))
+    add_handler(lambda: run(iptables_rule_out))
+    add_handler(lambda: run(iptables_rule_in))
 
 
 def stop_service(name: str, iface: str, config_files: list[str]):
